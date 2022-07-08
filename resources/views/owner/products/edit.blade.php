92c69d9df1f2d1708f489367b761b595282a01e6
daisukeyamachika <98957992+daisukeyamachika@users.noreply.github.com>
2022-07-08 22:19:40 +0900

    productDestroy

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -102,13 +102,21 @@
                             </div>
                         </div>
                     </form>
+                    <form id="delete_{{$product->id}}" method="post" action="{{ route('owner.products.destroy', ['product' => $product->id]) }}">
+                        @csrf
+                        @method('delete')
+                        <div class="p-2 w-full flex justify-around mt-32">
+                            <a href="#" data-id="{{$product->id}}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded">削除する</a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 
 <script>
-    'use strict'
+    'use strict';
+
     const images = document.querySelectorAll('.image') //全てのimageタグを取得
     images.forEach(image => { // 1つずつ繰り返す
         image.addEventListener('click', function(e){ // クリックしたら
@@ -123,6 +131,13 @@
             MicroModal.close(modal); //モーダルを閉じる
         })
     })
+
+    function deletePost(e) {
+        'use strict';
+        if (confirm('本当に削除してもいいですか?')) {
+            document.getElementById('delete_' + e.dataset.id).submit();
+        }
+    }
 </script>
 
 </x-app-layout>
